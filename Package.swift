5eeacf8146514632702c92968e6662bab5f0c350
Muukii <muukii.app@gmail.com>
2022-10-18 22:54:36 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -4,29 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "SerialDSL",
-    platforms: [
-      .macOS(.v10_13),
-      .iOS(.v13),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SerialDSL",
-            targets: ["SerialDSL"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SerialDSL",
-            dependencies: []),
-        .testTarget(
-            name: "SerialDSLTests",
-            dependencies: ["SerialDSL"]),
-    ]
+  name: "SerialDSL",
+  platforms: [
+    .macOS(.v10_13),
+    .iOS(.v13),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "SerialDSL",
+      targets: ["SerialDSL"]
+    )
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "SerialDSL",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "SerialDSLTests",
+      dependencies: ["SerialDSL"]
+    ),
+  ]
 )
