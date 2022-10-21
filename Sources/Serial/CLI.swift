0fb8f80e957786752e6eda033ed4e25901bca002
Muukii <muukii.app@gmail.com>
2022-10-21 15:19:53 +0900

    :evergreen_tree: Update

--- Sources/Serial/CLI.swift
+++ Sources/Serial/CLI.swift
@@ -70,7 +70,7 @@ struct CLI: AsyncParsableCommand {
       }
 
       guard localFileSystem.exists(libraryPath) else {
-        print(applicationPath, runtimeFrameworksPath, libraryPath)
+        print(CommandLine.arguments[0])
         throw CLIError.runtimeNotFound
       }
 
