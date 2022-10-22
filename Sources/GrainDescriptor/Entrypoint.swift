e0c4ef3da0501b55aed0e4a6674bb1880cbff0e6
Hiroshi Kimura <muukii.app@gmail.com>
2022-10-22 23:39:31 +0900

    Add parameter to override file-extension (#11)

--- Sources/GrainDescriptor/Entrypoint.swift
+++ Sources/GrainDescriptor/Entrypoint.swift
@@ -1,60 +1,60 @@
-@_implementationOnly import Darwin.C
-
-@_exported import Foundation
 @_exported import Alamofire
+@_implementationOnly import Darwin.C
+@_exported import Foundation
 @_exported import TSCBasic
 
 public struct Serialization {
-    
+
   private let _encode: (any GrainView) throws -> Data
   public let fileExtension: String
-  
+
   public init(fileExtension: String, encode: @escaping (any GrainView) throws -> Data) {
     self.fileExtension = fileExtension
     self._encode = encode
   }
-  
+
   public static var json: Self {
-    
+
     return .json(
       outputFormatting: [.prettyPrinted, .sortedKeys],
       dateEncodingStrategy: .iso8601,
       dataEncodingStrategy: .base64
     )
-    
+
   }
-  
+
   public static func json(
     outputFormatting: JSONEncoder.OutputFormatting,
     dateEncodingStrategy: JSONEncoder.DateEncodingStrategy,
     dataEncodingStrategy: JSONEncoder.DataEncodingStrategy
   ) -> Self {
-    
+
     let encoder = JSONEncoder()
-    
+
     encoder.outputFormatting = outputFormatting
     encoder.dateEncodingStrategy = dateEncodingStrategy
     encoder.dataEncodingStrategy = dataEncodingStrategy
-    
+
     return .init(
       fileExtension: "json",
       encode: {
         try encoder.encode($0)
-    })
-    
+      }
+    )
+
   }
-  
+
   public func encode(_ view: some GrainView) throws -> Data {
     return try _encode(view)
   }
-    
+
 }
 
 public struct Context: Codable {
-  
+
   public let filePath: AbsolutePath
   public let outputDir: AbsolutePath?
-  
+
   public init(
     filePath: AbsolutePath,
     outputDir: AbsolutePath?
@@ -62,86 +62,86 @@ public struct Context: Codable {
     self.filePath = filePath
     self.outputDir = outputDir
   }
-  
+
   public func json() -> String {
     let e = JSONEncoder()
     let d = try! e.encode(self)
     return String(data: d, encoding: .utf8)!
   }
-    
+
   public static func decode(_ data: Data) -> Self {
     let d = JSONDecoder()
     let r = try! d.decode(Self.self, from: data)
     return r
   }
-  
+
   public func write(data: Data, into path: AbsolutePath) throws {
     let url = URL(fileURLWithPath: path.pathString)
     try data.write(to: url, options: [.atomic])
   }
-    
+
 }
 
 public struct Output {
-  
+
   public enum FileOption {
     case auto
-    case named(String)
+    case named(String, fileExtension: String? = nil)
   }
-  
+
   private let _run: (Context, Data, String) throws -> Void
-  
+
   public init(_ run: @escaping (Context, Data, String) throws -> Void) {
     self._run = run
   }
-  
+
   func run(context: Context, data: Data, fileExtension: String) throws {
     try _run(context, data, fileExtension)
   }
-  
+
   public static var stdout: Self {
     .init { _, data, _ in
       let text = String(data: data, encoding: .utf8)!
       print(text)
     }
   }
-  
+
   public static func file(path: AbsolutePath) -> Self {
     .init { context, data, _ in
       try context.write(data: data, into: path)
       Log.info("Write -> \(path)")
     }
   }
-  
+
   public static var file: Self {
     .file(.auto)
   }
-  
+
   public static func file(_ option: FileOption) -> Self {
     .init { context, data, fileExtension in
-      
+
       let basePath: AbsolutePath
-      
+
       if let outputDir = context.outputDir {
         basePath = outputDir
       } else {
         basePath = context.filePath.parentDirectory
       }
-      
+
       let fileName: String
       switch option {
-      case .auto:    
+      case .auto:
         fileName = [context.filePath.basenameWithoutExt, fileExtension].joined(separator: ".")
-      case .named(let name):
-        fileName = [name, fileExtension].joined(separator: ".")
+      case .named(let name, let overrideFileExtension):
+        fileName = [name, (overrideFileExtension ?? fileExtension)].joined(separator: ".")
       }
-                       
+
       let path = basePath.appending(component: fileName)
       try context.write(data: data, into: path)
       Log.info("Write -> \(path)")
     }
   }
-   
+
 }
 
 public func serialize(
@@ -149,7 +149,7 @@ public func serialize(
   output: Output = .stdout,
   @GrainBuilder _ thunk: () throws -> some GrainView
 ) {
-  
+
   do {
     let value = try thunk()
     let data = try serialization.encode(value)
